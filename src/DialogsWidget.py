b609c035f8f8afec58e57d9e1528704f13be70f3
Ilya Ilyinykh <IlyasYOY@gmail.com>
2018-03-29 00:45:51 +0300

    Some redundant features were removed

--- src/DialogsWidget.py
+++ src/DialogsWidget.py
@@ -6,19 +6,15 @@ from PyQt5.QtCore import QSize
 class DialogsWidget(QWidget):
     log = logging.getLogger(name='DialogsWidget')
     dialogs = None
-    findDialogButton = None
     updateButton = None
     userId = None
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.dialogs = QListWidget(self)
-        self.findDialogButton = QPushButton(self)
         self.updateButton = QPushButton(self)
         self.updateButton.setText('Update dialogs.')
-        self.findDialogButton.setText('Start Dialog')
         self.dialogs.setIconSize(QSize(50, 50))
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.dialogs)
-        self.layout().addWidget(self.findDialogButton)
         self.layout().addWidget(self.updateButton)
